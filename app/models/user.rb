1521fe0895d4bb610964d492937b83e0b5ca2101
Poliard Djimmy <poliarddjimmy@gmail.com>
2022-05-24 14:53:25 -0400

    generate user scaffold

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# User model
+class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validate do |user|
+    user.errors.add(:base, 'Invalide email') if EMAIL_REGEX.match(user.email).nil?
+    user.errors.add(:base, 'The Password must be at least 6 characters') if user.password && user.password.length < 6
+    user.errors.add(:base, 'The email is Required') if user.email.blank?
+    user.errors.add(:base, 'The email is already taken') if User.where(email: user.email).exists?
+  end
+end
