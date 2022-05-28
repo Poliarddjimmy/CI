6ad760532c942db9148b20241976f3b4b5e36337
Poliard Djimmy <poliarddjimmy@gmail.com>
2022-05-27 21:10:46 -0400

    add data to display in the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  before_action :rooted
+
   def home
+    @contacts = current_user.contacts.where(failed_reason: nil).page(params[:page]).per(5)
+    @contact_files = current_user.contact_files.page(params[:page]).per(5)
   end
 end
